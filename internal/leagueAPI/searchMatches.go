e1c5242992a726745dbe67b3b7372ceca89f5854
UnbreakablePotato <swipeytwit@gmail.com>
2026-07-21 09:55:44 +0200

    Cleaned each match id more thoroughly as it was causing the first and last match id to carry square brackets, as well as every match id having quotation marks, oops.
    Finished ShowShallowMatch

--- internal/leagueAPI/searchMatches.go
+++ internal/leagueAPI/searchMatches.go
@@ -2,6 +2,7 @@ package leagueapi
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -18,9 +19,12 @@ var Items = make(map[int]string)
 type ShallowMatch struct {
 	Info struct {
 		GameDuration int `json:"gameDuration"`
-		Participants struct {
+		Participants []struct {
 			ChampLevel   int    `json:"champLevel"`
 			ChampionName string `json:"championName"`
+			Deaths       int    `json:"deaths"`
+			Kills        int    `json:"kills"`
+			Assists      int    `json:"assists"`
 		} `json:"participants"`
 	} `json:"info"`
 }
@@ -28,11 +32,12 @@ type ShallowMatch struct {
 type DeepMatch struct {
 	Info struct {
 		GameDuration int `json:"gameDuration"`
-		Participants struct {
+		Participants []struct {
 			ChampLevel         int    `json:"champLevel"`
 			ChampionName       string `json:"championName"`
 			Deaths             int    `json:"deaths"`
 			Kills              int    `json:"kills"`
+			Assists            int    `json:"assists"`
 			GoldEarned         int64  `json:"goldEarned"`
 			IndividualPosition string `json:"individualPosition"`
 			Item0              int    `json:"item0"`
@@ -77,7 +82,7 @@ func SearcMatchID(region string, puuid string) []string {
 	}
 
 	fullUrl := "https://" + region + ".api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid + "/ids?start=0&count=5&api_key=" + apiKey
-
+	//fmt.Printf("%s\n", fullUrl)
 	res, err := http.Get(fullUrl)
 	if err != nil {
 		fmt.Printf("matchId request failed: %v\n", err)
@@ -93,7 +98,10 @@ func SearcMatchID(region string, puuid string) []string {
 	result := strings.Split(interResult, ",")
 
 	for i := range result {
-		fmt.Printf("debug: %s", result[i])
+		result[i] = strings.ReplaceAll(result[i], "\"", "")
+		result[i] = strings.ReplaceAll(result[i], "[", "")
+		result[i] = strings.ReplaceAll(result[i], "]", "")
+		//fmt.Printf("debug: %s\n", result[i])
 	}
 
 	return result
@@ -106,8 +114,77 @@ and perhaps summonors for each
 then a seperate command where you can look at just one match in far more detail
 */
 
-func ShowShallowMatch(matchIds []string) {
+var shallow ShallowMatch
 
+func ShowShallowMatch(matchIds []string) error {
+
+	for i := range matchIds {
+		id := matchIds[i]
+
+		fullUrl := "https://" + Usr.Region + ".api.riotgames.com/lol/match/v5/matches/" + id + "?api_key=" + apiKey
+		//https://europe.api.riotgames.com/lol/match/v5/matches/EUW1_7922609631?api_key=
+		//fmt.Printf("%s\n", fullUrl)
+		res, err := http.Get(fullUrl)
+		if err != nil {
+			fmt.Printf("Get rquest failed when requesting match id: %s, error: %v", id, err)
+			return errors.New("")
+
+		}
+
+		if res.StatusCode != http.StatusOK {
+			fmt.Printf("Status code is not OK: %d\n", res.StatusCode)
+			return errors.New("")
+		}
+
+		defer res.Body.Close()
+
+		data, err := io.ReadAll(res.Body)
+		if err != nil {
+			fmt.Printf("io.ReadAll failed in ShowShallowFunction: %v", err)
+			return errors.New("")
+		}
+
+		if err := json.Unmarshal(data, &shallow); err != nil {
+			fmt.Printf("Unmarshal failed in ShowShallowMatch: %v", err)
+			return errors.New("")
+		}
+
+		fmt.Println("-----------------------------------------------")
+		fmt.Print("-----------------------------------------------\n\n")
+
+		fmt.Printf("MATCHID: %s\n\n", id)
+
+		fmt.Printf("%s ---------------------------------------- %s\n", shallow.Info.Participants[0].ChampionName, shallow.Info.Participants[5].ChampionName)
+
+		fmt.Printf("K: %d D: %d A: %d ---- K: %d D: %d A: %d\n\n", shallow.Info.Participants[0].Kills, shallow.Info.Participants[0].Deaths, shallow.Info.Participants[0].Assists,
+			shallow.Info.Participants[5].Kills, shallow.Info.Participants[5].Deaths, shallow.Info.Participants[5].Assists)
+
+		fmt.Printf("%s ---------------------------------------- %s\n", shallow.Info.Participants[1].ChampionName, shallow.Info.Participants[6].ChampionName)
+
+		fmt.Printf("K: %d D: %d A: %d ---- K: %d D: %d A: %d\n\n", shallow.Info.Participants[1].Kills, shallow.Info.Participants[1].Deaths, shallow.Info.Participants[1].Assists,
+			shallow.Info.Participants[6].Kills, shallow.Info.Participants[6].Deaths, shallow.Info.Participants[6].Assists)
+
+		fmt.Printf("%s ---------------------------------------- %s\n", shallow.Info.Participants[2].ChampionName, shallow.Info.Participants[7].ChampionName)
+
+		fmt.Printf("K: %d D: %d A: %d ---- K: %d D: %d A: %d\n\n", shallow.Info.Participants[2].Kills, shallow.Info.Participants[2].Deaths, shallow.Info.Participants[2].Assists,
+			shallow.Info.Participants[7].Kills, shallow.Info.Participants[7].Deaths, shallow.Info.Participants[7].Assists)
+
+		fmt.Printf("%s ---------------------------------------- %s\n", shallow.Info.Participants[3].ChampionName, shallow.Info.Participants[8].ChampionName)
+
+		fmt.Printf("K: %d D: %d A: %d ---- K: %d D: %d A: %d\n\n", shallow.Info.Participants[3].Kills, shallow.Info.Participants[3].Deaths, shallow.Info.Participants[3].Assists,
+			shallow.Info.Participants[8].Kills, shallow.Info.Participants[8].Deaths, shallow.Info.Participants[8].Assists)
+
+		fmt.Printf("%s ---------------------------------------- %s\n", shallow.Info.Participants[4].ChampionName, shallow.Info.Participants[9].ChampionName)
+
+		fmt.Printf("K: %d D: %d A: %d ---- K: %d D: %d A: %d\n", shallow.Info.Participants[4].Kills, shallow.Info.Participants[4].Deaths, shallow.Info.Participants[4].Assists,
+			shallow.Info.Participants[9].Kills, shallow.Info.Participants[9].Deaths, shallow.Info.Participants[9].Assists)
+
+		fmt.Println("-----------------------------------------------")
+		fmt.Print("-----------------------------------------------\n\n")
+
+	}
+
+	return nil
 }
 
 func ShowDeepMatch(matchId string) {
@@ -126,7 +203,7 @@ func CheckCurrentGameInfo() bool {
 	}
 
 	fullUrl := "https://" + region + ".api.riotgames.com/lol/spectator/v5/active-games/by-summoner/" + Usr.Puuid
-
+	//fmt.Printf("%s\n", fullUrl)
 	res, err := http.Get(fullUrl)
 	if err != nil {
 		fmt.Printf("Get request failed to show current game: %v\n", err)
